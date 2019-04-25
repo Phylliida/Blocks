1d9d31ec69d26d8c2d967a2761451cabb97f7358
Phylliida <phylliidadev@gmail.com>
2019-04-25 12:16:29 +0100

    In last commit i meant that chunks can be different sizes in the x dimension, y dimension, and z dimension now

---
+++ Blocks/Assets/Blocks/gui/TimeSeries.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSeries : MonoBehaviour
+{
+
+    float[] data;
+
+    public bool modified = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (data != null && !modified)
+        {
+        }
+    }
+}
